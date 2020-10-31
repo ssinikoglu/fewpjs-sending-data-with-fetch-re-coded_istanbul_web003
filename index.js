480d09e9c6940a0cec537d82d9c851fe381e56ab
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-10-31 17:38:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((object) => {
+      document.body.innerHTML += `<p><b>UserName:</b>${object.name}<br><b>E-mail:</b>${object.email}<br><b>UserId:</b>${object.id}</p>`;
+    })
+    .catch(function (error) {
+      alert("Error! But do not forget, persistance powers passion!");
+      document.body.innerHTML = error.message;
+    });
+}
+
+submitData("Sukare", "sukare@sukare.com");
